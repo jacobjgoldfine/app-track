8d7db1e8f3c316008ade1868ce56189c20f006e5
grace butler <gmx.butler@gmail.com>
2021-10-06 11:58:53 -0500

    added bcrypt and jsonwebtolken to package.json file added utils folder and auth file to server side so that we can use jwt added auth file to server side added react-router-dom and jwt-decode to client side package.json so that we can use it for the sign up page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,10 +5,10 @@ const Header = () => {
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <h1 className="m-0" style={{ fontSize: '3rem' }}>
-          Tech Friends
+          App Track
         </h1>
         <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
+          Meet your new Job Application Tracker
         </p>
       </div>
     </header>
