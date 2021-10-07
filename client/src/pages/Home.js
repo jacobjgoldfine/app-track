f1c9ed8c0c195177b461e2b1d900b3db611dda3d
Jennifer Davila <pazjenni1331@gmail.com>
2021-10-06 20:03:33 -0500

    Added a summary to the home-page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm';
-
+import React from "react";
+import LoginForm from "../components/LoginForm";
 
 const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
-        <LoginForm/> 
+        <div className="flex-row justify-center">
+          <p id="home-paragraph">
+            Organization is the key to success! Create an account to keep track,
+            organize, and sort all the employers you have applied to. Keeping
+            your eye on the prize will allow you to easily see your progress.
+            Good luck!{" "}
+          </p>
+        </div>
+        <LoginForm />
       </div>
     </main>
   );
