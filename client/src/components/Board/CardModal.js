5667bf3df47d615f8a32c209634d553779fe64ac
Jennifer Davila <pazjenni1331@gmail.com>
2021-10-12 23:14:12 -0500

    removed button

--- client/src/components/Board/CardModal.js
+++ client/src/components/Board/CardModal.js
@@ -2,7 +2,6 @@
 // import { Link } from "react-router-dom";
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useQuery } from "@apollo/client";
