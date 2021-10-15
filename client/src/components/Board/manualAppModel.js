a6a37ef72b20cf9e3a20207d13fed5b5eced6758
Jennifer Davila <pazjenni1331@gmail.com>
2021-10-15 17:14:40 -0500

    fixed window size

--- client/src/components/Board/manualAppModel.js
+++ client/src/components/Board/manualAppModel.js
@@ -14,7 +14,7 @@ const style2 = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 1000,
-  height: 500,
+  height: 650,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
