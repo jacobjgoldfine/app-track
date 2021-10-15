cbfb3c3c27cd01f5a333c7fa575fbb03fa96c73c
Marc Moten <marcmotenii@gmail.com>
2021-10-14 21:30:23 -0500

    deleted name on payload

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,8 +28,8 @@ module.exports = {
     // return the request object so it can be passed to the resolver as `context`
     return req;
   },
-  signToken: function ({ email, name, _id }) {
-    const payload = { email, name, _id };
+  signToken: function ({ email, _id }) {
+    const payload = { email, _id };
     console.log(payload);
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     //jwt.sign is what we send back
